dd2224dee2c141e3cf683db27fdc4920f87cd46f
Jeffry Orthober <jeffryorthober@Jeffrys-MacBook-Pro.local>
2017-10-29 13:09:24 -0600

    Hello World endpoint /greeting

---
+++ src/main/java/orthober/jeff/STSCampLogic/Greeting.java
@@ -0,0 +1,26 @@
+package orthober.jeff.STSCampLogic;
+
+/**
+ * Domain object
+ * @author jeffryorthober
+ *
+ */
+public class Greeting {
+
+	private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+	
+}
---
+++ src/main/java/orthober/jeff/STSCampLogic/GreetingController.java
@@ -0,0 +1,29 @@
+package orthober.jeff.STSCampLogic;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller
+ * @author jeffryorthober
+ *
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    
+    //A long variable which can be read and written atomically
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = RequestMethod.GET, value = "/greeting")
+    public Greeting greeting(
+    		@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    		System.out.println("Just got a GET request.");
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
